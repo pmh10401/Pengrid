c0fd61b4af8a42749d6870747eb6de4fc7ec5026
pmh10401 <39891182+pmh10401@users.noreply.github.com>
2026-08-14 08:15:32 +0900

    build: advance developer preview build

--- Tests/BloomFileManagerTests/BuildScriptTests.swift
+++ Tests/BloomFileManagerTests/BuildScriptTests.swift
@@ -281,7 +281,7 @@ import Testing
         encoding: .utf8
     )
 
-    for declaration in ["APP_VERSION=\"1.3.0\"", "BUILD_VERSION=\"8\""] {
+    for declaration in ["APP_VERSION=\"1.3.0\"", "BUILD_VERSION=\"9\""] {
         #expect(development.contains(declaration))
         #expect(release.contains(declaration))
     }
